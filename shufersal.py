e639602c6902988022bc86a308d8ef3e780042b6
Arthur Flam <arthur.flam@gmail.com>
2022-06-05 18:19:59 +0300

    More shufersal data: allergen traces, health warnings, healthier alternatives, nutrition facts, categories...

--- shufersal.py
+++ shufersal.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 from pathlib import Path
 
 import httpx
@@ -57,32 +58,111 @@ def barcode_metadata(barcode: str, no_cache=False):
     if barcode in shufersal_db and not no_cache:
         return shufersal_db[barcode]
 
-    cache_dir = Path(os.environ['KASPENU_CACHE_DIR']) / 'shufersal'
+    cache_dir = Path(os.environ.get('KASPENU_CACHE_DIR', '.cache')) / 'shufersal'
     cache_dir.mkdir(exist_ok=True, parents=True) # TODO: more to init
-    cache_path = cache_dir / f'P_{barcode}.json'
+    cache_path = cache_dir / f'P_{barcode}.html'
     if cache_path.exists():
         print(f'HIT {cache_path}')
-        text = cache_path.read_text()
+        # FIXME: on windows it's a mess, we should not encoding= 
+        text = cache_path.read_text(encoding='utf-8', errors='surrogateescape')
     else:
         url = f"https://www.shufersal.co.il/online/he/p/P_{barcode}/json"
+        print(f"MISS {url}")
         r = httpx.get(url, params={"cardContext[openFrom]": "CATEGORY"}, verify=False)
         r.raise_for_status()
         if "Forcepoint" in r.text: raise ValueError("Forcepoint")
-        text = r.text
-        cache_path.write_text(text)
+        if "Shufersal - Error" in r.text: raise ValueError("Error from Shufersal - doenst want us to scrap...")
+        if not r.text: raise ValueError("Empty")
+        text = r.content
+        cache_path.write_bytes(text)
+
+    # TODO: align with https://wiki.openfoodfacts.org/API/Full_JSON_example
+    data = {}
 
     soup = BeautifulSoup(text, 'html.parser')
-    data = {}
+
+    element = soup.find("div", {"class": "modal-dialog"})
+    meta_str = element.get('data-gtm', '{}')
+    meta = json.loads(meta_str)
+    # print(meta)
+    data["name"] = meta["productName"] # nescafe capuccino classic vanilla
+    data["brand"] = meta["brand"] # nestle
+    data["categoryID"] = meta["categoryID"] # A130504
+    data["categoryCodes"] = meta["categoryCodes"] # ["A","A13","A1305","A130504"]
+    # TODO: crawl those once and for all...
+    data["categoryLevel1"] = meta["categoryLevel1"] # supermarket
+    data["categoryLevel2"] = meta["categoryLevel2"] # drinks, alcohol, wine
+    data["categoryLevel3"] = meta["categoryLevel3"] # coffee and tea
+    data["categoryLevel4"] = meta["categoryLevel4"] # soluble coffee, chocolate mix...
+
     images = soup.find_all("img", itemprop="image")
     data['images'] = [i.get('src') for i in images]
-    details = soup.find("li", id="techDetails")
-    # data['details'] = details
+
+    remarks = soup.find('div', {"class": "remarksText"})
+    if remarks:
+        data['description'] = remarks.get_text("\n", strip=True)
+
+    remap = {
+        "מותג/יצרן": "producer_name",
+        "ארץ ייצור": "producer_country",
+        "כשרות": "kosher_authority", # OU
+        "חלבי/בשרי/פרווה": "kosher_type", # חלבי
+        "פסח": "kosher_pessah", # לא לפסח
+    }
+    misc_root = soup.find("ul", {"class": "dataList"})
+    if misc_root:
+        misc_meta = {}
+        for e in misc_root.find_all('li'):
+            key = e.find("div", {"class": "name"}).text.replace(":", "")
+            if key in ('מק"ט', 'מידה/סוג'):
+                continue
+            value = e.find("div", {"class": "text"}).text
+            misc_meta[remap.get(key, key)] = value
+        # print(misc_meta)
+        data.update(misc_meta)
+
     ingredients = soup.find('div', {"class": "componentsText"})
     if ingredients:
         data['ingredients'] = clean(ingredients.get_text())
+
+    allergens = soup.find('div', {"class": "alergiesProperties"})
+    if allergens:
+        data['allergens'] = clean(allergens.get_text())
+    allergens_traces = soup.find('div', {"class": "alergiesTracesProperties"})
+    if allergens_traces:
+        data['allergens_traces'] = clean(allergens_traces.get_text())
     components = soup.find('div', {"class": "featuresList"})
     if components:
         data['components'] = clean(components.get_text())
+    markings = soup.find('div', {"class": "markingList"})
+    if markings:
+        data['health'] = [
+            e.find('div', {"class": "text"}).text
+            for m in markings.find_all('div', {"class": "markingItem"}) 
+        ]
+
+    product_info = soup.find('ul', {"class": "productContainer"})
+    for li in product_info.find_all('li'):
+        nutrition_title = li.find('div', {"id": "nutritionListTitle"})
+        if nutrition_title:
+            if 'nutrition' not in data:
+                data["nutrition"] = {}
+            serving = clean(li.find('div', {"class": "subInfo"}).get_text())
+            data["nutrition"][serving] = []
+            for el in li.find('div', {"class": "nutritionList"}).find_all('div', {"class": "nutritionItem"}):
+                unit = el.find('div', {"class": "name"}).get_text(strip=True)
+                unit_remap = {
+                    "מג": "mg",
+                    "גרם": "g",
+                    "קל": "kg",
+                }
+                unit = unit_remap.get(unit, unit)
+                data['nutrition'][serving].append({
+                    "element": el.find('div', {"class": "text"}).get_text(strip=True),
+                    "value": el.find('div', {"class": "number"}).get_text(strip=True),
+                    "unit": unit,
+                })
+
     warnings = soup.find('li', {"class": "productSymbols"})
     if warnings:
         images = warnings.find_all("img")
@@ -91,6 +171,11 @@ def barcode_metadata(barcode: str, no_cache=False):
         # 320 => sodium
         # 321 => sugar
         # 322 => fat
+
+    healthier = soup.find('section', {"class": "healthier"})
+    if healthier:
+        data['healthier'] = [e.get("data-product-code").replace("P_", "") for e in healthier.find_all("div", {"class": "SEARCH"})]
+
     shufersal_db[barcode] = data
     shufersal_db.commit()
     return data
--- tests/test_shufersal.py
+++ tests/test_shufersal.py
@@ -7,16 +7,97 @@ from shufersal import barcode_metadata
 
 
 products = [
-    ('8801055707010', {
-        'name': 'nescafe cappuccino classic',
-    }),
+    # ('8801055707010', {
+    #     'name': 'nescafe cappuccino classic',
+    # }),
+    ('3387390525960', {
+       'name': 'xxx'
+    })
 ]
+
+### Today we get this:
+# {'allergens': 'חלב',
+#  'brand': 'נסקפה',
+#  'categoryCodes': ['A', 'A13', 'A1305', 'A130504'],
+#  'categoryID': 'A130504',
+#  'categoryLevel1': 'סופרמרקט',
+#  'categoryLevel2': 'משקאות, אלכוהול ויין',
+#  'categoryLevel3': 'קפה ותה',
+#  'categoryLevel4': 'קפה נמס, שוקו ומלבין ',
+#  'description': "נסקפה קפוצ'ינו קלאסי הוא שילוב בין תערובת חלב מפנקת, קפה מבית "
+#                 'נסקפה ושכבת קצף אוורירית\n'
+#                 '• מיוצר מתערובת פולי קפה שנבחרו בקפידה ונקלו קלות\n'
+#                 '• רק מוזגים מים חמים והמשקה מוכן!\n'
+#                 '• האריזה מכילה 10 מנות אישיות\n'
+#                 '• כל שלב בהפקת קפה זה נעשה תוך הקפדה על קיימות וכבוד לסביבה, '
+#                 'לחקלאים ולקהילה',
+#  'healthier': ['3903788', '3903771', '8801055706976', '8801055706952'],
+#  'images': ['https://res.cloudinary.com/shufersal/image/upload/f_auto,q_auto/v1551800922/prod/product_images/products_zoom/LAH42_Z_P_8801055707010_1.png',
+#             'https://res.cloudinary.com/shufersal/image/upload/f_auto,q_auto/v1551800922/prod/product_images/products_small/LAH42_S_P_8801055707010_1.png'],
+#  'ingredients': 'אבקת חלב כחוש, שמן צמחי מוקשה, מלטודקסטרין, קפה נמס (15%), '
+#                 'לקטוז, סוכר, מייצבים (E331, E452, E340), קזאינט, מלח, חומר '
+#                 'מונע גיוש (E5 51), שמן קוקוס.',
+#  'kosher_authority': 'OU',
+#  'kosher_pessah': 'לא לפסח',
+#  'kosher_type': 'חלבי',
+#  'name': "נסקפה קפוצ'ינו קלאסיק",
+#  'nutrition': {'100 גרם': [{'element': 'סוכרים מתוך פחמימות',
+#                             'unit': 'g',
+#                             'value': '28.8'},
+#                            {'element': 'אנרגיה', 'unit': 'kg', 'value': '497'},
+#                            {'element': 'חלבונים', 'unit': 'g', 'value': '13.9'},
+#                            {'element': 'פחמימות', 'unit': 'g', 'value': '55.1'},
+#                            {'element': 'שומנים', 'unit': 'g', 'value': '24.5'},
+#                            {'element': 'כולסטרול', 'unit': 'mg', 'value': '6'},
+#                            {'element': 'נתרן', 'unit': 'mg', 'value': '500'},
+#                            {'element': 'מתוכם שומן רווי',
+#                             'unit': 'g',
+#                             'value': '20.9'},
+#                            {'element': 'חומצות שומן טרנס',
+#                             'unit': 'g',
+#                             'value': 'פחות מ 0.5'},
+#                            {'element': 'כפיות סוכר',
+#                             'unit': '',
+#                             'value': '7.25'}],
+#                '100 מ"ל מוכן': [{'element': 'סוכרים מתוך פחמימות',
+#                                  'unit': 'g',
+#                                  'value': '1.7'},
+#                                 {'element': 'אנרגיה',
+#                                  'unit': 'kg',
+#                                  'value': '30'},
+#                                 {'element': 'מתוכם שומן רווי',
+#                                  'unit': 'g',
+#                                  'value': '1.2'},
+#                                 {'element': 'חומצות שומן טרנס',
+#                                  'unit': 'g',
+#                                  'value': 'פחות מ 0.5'}],
+#                'מנה': [{'element': 'סוכרים מתוך פחמימות',
+#                         'unit': 'g',
+#                         'value': '3.6'},
+#                        {'element': 'אנרגיה', 'unit': 'kg', 'value': '62'},
+#                        {'element': 'חלבונים', 'unit': 'g', 'value': '1.7'},
+#                        {'element': 'פחמימות', 'unit': 'g', 'value': '6.9'},
+#                        {'element': 'שומנים', 'unit': 'g', 'value': '3.1'},
+#                        {'element': 'כולסטרול', 'unit': 'mg', 'value': '0.8'},
+#                        {'element': 'נתרן', 'unit': 'mg', 'value': '63'},
+#                        {'element': 'מתוכם שומן רווי',
+#                         'unit': 'g',
+#                         'value': '2.6'},
+#                        {'element': 'חומצות שומן טרנס',
+#                         'unit': 'g',
+#                         'value': 'פחות מ 0.5'},
+#                        {'element': 'כפיות סוכר', 'unit': '', 'value': '1'}]},
+#  'producer_country': 'דרום קוריאה',
+#  'producer_name': 'נסקפה',
+#  'אבקת חלב נוכרי': 'לא'}
+
 
 class TestShufersalMetadata(unittest.TestCase):
   def test_is_int(self):
     barcode, nescafe_data = products[0]
     data = barcode_metadata(barcode, no_cache=True)
-    print(data)
+    from pprint import pprint
+    pprint(data)
     # self.assertEqual(bool(is_int("4")), True)
 
 
